b2d00160239f4dc2311b5fc2ece56942edfba48e
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-03-02 10:18:04 +0300

    Make OsString support more prominent

---
+++ examples/non-utf8.rs
@@ -0,0 +1,24 @@
+use std::{ffi::OsString, os::unix::ffi::OsStringExt};
+
+mod flags {
+    use std::{ffi::OsString, path::PathBuf};
+
+    xflags::args! {
+        cmd Cmd
+            required a: OsString
+            required b: PathBuf
+            required c: String
+        {
+        }
+    }
+}
+
+fn main() {
+    let flags = flags::Cmd::from_vec(vec![
+        OsString::from_vec(vec![254].into()),
+        OsString::from_vec(vec![255].into()),
+        "utf8".into(),
+    ]);
+
+    eprintln!("flags = {:?}", flags);
+}
