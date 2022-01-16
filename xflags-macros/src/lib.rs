4943a55dfbb180a799d3f604e434245aa901f87f
Laurențiu Nicola <lnicola@dend.ro>
2022-01-16 17:06:06 +0200

    Don't gate on not-test

--- xflags-macros/src/lib.rs
+++ xflags-macros/src/lib.rs
@@ -3,7 +3,6 @@ mod parse;
 mod emit;
 mod update;
 
-#[cfg(not(test))]
 #[proc_macro]
 pub fn xflags(ts: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let cmd = parse::parse(ts).unwrap();
